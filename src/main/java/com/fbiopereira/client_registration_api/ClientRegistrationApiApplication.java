9e915b18fab6f244ba79ac69564d45d45ecc0cf0
Fábio Pereira <fabio.pereira@outlook.com>
2024-07-16 20:19:51 -0300

    Project Structure

---
+++ src/main/java/com/fbiopereira/client_registration_api/ClientRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.fbiopereira.client_registration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegistrationApiApplication.class, args);
+	}
+
+}
